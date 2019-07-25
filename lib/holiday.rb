32ec1761a29b3a975061781ec4ba5081f5907780
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-25 15:26:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key.to_s.capitalize!}:"
     holiday_hash[key].each do |key, value|
-      puts value.to_s
+      puts "  #{key.to_s.capitalize!}:"
       value.each do |index|
         all_supplies << index
       end
